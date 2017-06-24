24342a0c2ae8053757e263aec657d21c6bb38512
Alex Smith <alexandersmith223@gmail.com>
2017-06-24 17:10:53 -0400

    Add sorting logic

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,8 +2,19 @@ class JobsController < ApplicationController
 
 	before_action :set_feed, only: :index
 
+
 	def index
-		@jobs = @feed.jobs.paginate(page: params[:page], per_page: 10)
+		order_param = params[:order_param]
+		case order_param
+		when 'title'
+			@jobs = @feed.jobs.order(title: :asc).paginate(page: params[:page], per_page: 10)
+		when 'published'
+			@jobs = @feed.jobs.order(published: :desc).paginate(page: params[:page], per_page: 10)
+		when 'department'
+			@jobs = @feed.jobs.order(author: :asc).paginate(page: params[:page], per_page: 10)
+		else
+			@jobs = @feed.jobs.paginate(page: params[:page], per_page: 10)
+		end
 	end
 
 	def show
@@ -15,4 +26,5 @@ class JobsController < ApplicationController
 		#hard coding the feed for OSU Jobs. When adding more feeds, replace one with params[:id]
 		@feed = Feed.find(1)
 	end
+
 end
